88130aefe88e2e95bb7e5b74fc090e684c408779
Edson de Senna Junior <edsonsjr14@outlook.com>
2023-02-05 19:39:19 -0300

    Starting login behavior

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,7 @@
 declare module '@env' {
+  export const MSAPP_WEB_URL: string;
   export const STRAVA_API_URL: string;
+  export const STRAVA_AUTH_URL: string;
+  export const STRAVA_CLIENT_ID: string;
+  export const STRAVA_CLIENT_SECRET: string;
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,33 @@
-import React, {Text, View} from 'react-native';
+import {MSAPP_WEB_URL, STRAVA_AUTH_URL, STRAVA_CLIENT_ID} from '@env';
+import React, {Button, Linking, Text, View} from 'react-native';
 import {Link} from 'react-router-native';
 
 const Login = () => {
+  const handleOnLoginClick = async () => {
+    try {
+      // const canOpenStravaApp = await Linking.canOpenURL(
+      //   'strava://oauth/mobile/authorize',
+      // );
+      // if (canOpenStravaApp) {
+      //   Linking.openURL('strava://oauth/mobile/authorize');
+      //   return;
+      // }
+
+      return Linking.openURL(
+        `${STRAVA_AUTH_URL}?client_id=${STRAVA_CLIENT_ID}&response_type=code&redirect_uri=${MSAPP_WEB_URL}&approval_prompt=force&scope=profile:read_all,activity:read_all`,
+      );
+    } catch (err) {
+      console.log('Error on click', {err});
+    }
+  };
+
   return (
     <View style={{paddingTop: 60, paddingLeft: 20}}>
       <Text>Workxxs!</Text>
       <Link to="/home">
         <Text>Home</Text>
       </Link>
+      <Button title="Login with Strava" onPress={handleOnLoginClick} />
     </View>
   );
 };
